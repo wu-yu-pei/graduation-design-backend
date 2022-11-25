4288964a23f4afef4199f9ae635261ba13d9d093
Wuyupei <495174699@qq.com>
2022-11-25 17:13:04 +0800

    fix:修改服务器端口号

--- src/index.ts
+++ src/index.ts
@@ -15,16 +15,10 @@ import useRouter from './router/index';
 
 const app: Koa = new Koa();
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, './config/https/7892505_wuyupei.top.key')),
-  cert: fs.readFileSync(path.join(__dirname, './config/https/7892505_wuyupei.top.pem')),
-};
-
-const server = https.createServer(options, app.callback());
 
 usePlugin(app);
 useRouter(app);
 
-server.listen(7552, () => {
+app.listen(7552, () => {
   console.log('server is runing at 7552');
 });
